f1d8ba8ab8b21abf4c816df69d98555fed73a367
“Rajat <rajatvermaa95@gmail.com>
2024-01-27 03:55:56 +0530

    #15: Configurations (Retries, Videos, Traces, Screenshots, Workers)

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
+const { on } = require('events');
 
 /**
  * Read environment variables from file.
@@ -18,13 +19,19 @@ module.exports = defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+ 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  /* Retry on CI only */
+  // retries: process.env.CI ? 2 : 0,
+  retries: 1, // Give failing tests 1 retry attempt(s)  
+  
+  /* Opt out of parallel tests on CI. */
+  // workers: process.env.CI ? 1 : undefined,
+  workers:6,
+
   use: {
 
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,7 +39,14 @@ module.exports = defineConfig({
     // BASE_URL=https://www-prod.saucedemo.com npx playwright test
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+
+    // video: 'retain-on-failure',
+    video: 'on',
+
+    // screenshot: 'only-on-failure',
+    screenshot: 'on',
+
   },
 
   /* Configure projects for major browsers */
