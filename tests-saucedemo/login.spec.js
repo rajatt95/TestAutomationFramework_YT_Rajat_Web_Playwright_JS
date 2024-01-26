7462b631a5f9beab9cec72b2df120288ef9fe747
“Rajat <rajatvermaa95@gmail.com>
2024-01-27 02:45:11 +0530

    #10: Annotations (Describe, BeforeEach)

--- tests-saucedemo/login.spec.js
+++ tests-saucedemo/login.spec.js
@@ -10,48 +10,57 @@ const {
   credentials_2: { invalid_username, invalid_password }
 } = loginCredentials.data;
 
+/**
+ * Test suite for Sauce Demo login functionality.
+ */
+test.describe('Sauce Demo - [LOGIN]', () => {
 
-test('[LOGIN] Login with valid credentials. Validate that User is able to login using valid credentials. @regression @sanity', async ({ page }) => {
+  /**
+   * Before each test, navigate to the homepage and create instances of Pages.
+   */
+  test.beforeEach(async({ page }) =>{
+    // Navigate to application
+    await page.goto('/')  
+  })
+
+  test('[LOGIN] Login with valid credentials. Validate that User is able to login using valid credentials. @regression @sanity', async ({ page }) => {
+   
+    // Fill valid credentials and Login 
+    await page.locator('[data-test="username"]').fill(valid_username)
+    await page.locator('[data-test="password"]').fill(valid_password)
+    await page.locator('[data-test="login-button"]').click()
   
-  // Navigate to application
-  await page.goto('/');
-
-  // Fill valid credentials and Login 
-  await page.locator('[data-test="username"]').fill(valid_username)
-  await page.locator('[data-test="password"]').fill(valid_password)
-  await page.locator('[data-test="login-button"]').click()
-
-  // Assertions for successful login
-
-  // Verify the heading on the Products page
-  await expect(page.locator('.title')).toHaveText('Products')
+    // Assertions for successful login
   
-  // Verify the logo on the header
-  await expect(page.locator('.app_logo')).toHaveText('Swag Labs')
-
-  // Verify the copyright message in the footer
-  await expect(page.locator('.footer_copy')).toContainText(' Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy')
-
-  // Verify that LinkedIn link in the footer is present
-  await expect(page.getByRole('link', { name: 'LinkedIn' })).toBeVisible()
-
-  // Verify the href attribute and value for the LinkedIn link in the footer
-  await expect(page.getByRole('link', { name: 'LinkedIn' })).toHaveAttribute('href', 'https://www.linkedin.com/company/sauce-labs/')
+    // Verify the heading on the Products page
+    await expect(page.locator('.title')).toHaveText('Products')
+    
+    // Verify the logo on the header
+    await expect(page.locator('.app_logo')).toHaveText('Swag Labs')
   
-});
-
-test('[LOGIN] Login with invalid credentials. Validate that User is unable to login using invalid credentials. @regression', async ({ page }) => {
+    // Verify the copyright message in the footer
+    await expect(page.locator('.footer_copy')).toContainText(' Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy')
   
-  // Navigate to application
-  await page.goto('/');
-
-  // Fill invalid credentials and Login 
-  await page.locator('[data-test="username"]').fill(invalid_username)
-  await page.locator('[data-test="password"]').fill(invalid_password)
-  await page.locator('[data-test="login-button"]').click()
-
-  // Verify the error message for Username and Password not match with any User
-  await expect(page.locator("//h3[contains(text(),'do not match')]")).toContainText('Username and password do not match')
+    // Verify that LinkedIn link in the footer is present
+    await expect(page.getByRole('link', { name: 'LinkedIn' })).toBeVisible()
   
+    // Verify the href attribute and value for the LinkedIn link in the footer
+    await expect(page.getByRole('link', { name: 'LinkedIn' })).toHaveAttribute('href', 'https://www.linkedin.com/company/sauce-labs/')
+    
+  });
+  
+  test('[LOGIN] Login with invalid credentials. Validate that User is unable to login using invalid credentials. @regression', async ({ page }) => {
+      
+    // Fill invalid credentials and Login 
+    await page.locator('[data-test="username"]').fill(invalid_username)
+    await page.locator('[data-test="password"]').fill(invalid_password)
+    await page.locator('[data-test="login-button"]').click()
+  
+    // Verify the error message for Username and Password not match with any User
+    await expect(page.locator("//h3[contains(text(),'do not match')]")).toContainText('Username and password do not match')
+    
+  });
+    
+
 });
 
