4c93a7345d3f00602a64c2d6576e367f9e58cf4d
“Rajat <rajatvermaa95@gmail.com>
2024-01-28 14:23:36 +0530

    #26: Added Utility for Wait

---
+++ utils/Waits.js
@@ -0,0 +1,24 @@
+class Waits{
+
+
+    /**
+     * Waits for a given amount of time.
+     *
+     * @param {number} timeInSeconds - The time to wait in seconds.
+     *
+     * @example
+     * // Example usage:
+     * await verificationManager.waitForGivenTime(5);
+     */
+    async waitForGivenTime(timeInSeconds){
+        console.log(`Waiting for '${timeInSeconds}' seconds.`)
+        await new Promise(resolve => setTimeout(resolve, timeInSeconds * 1000));
+    }
+
+}
+
+/**
+ * Exports the Waits class as the default export of this module.
+ * @module Waits
+ */
+export default new Waits;
